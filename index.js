9d548f599a0b9e910057feb2cfb04cb561764637
amaorine <c0112548d8@edu.teu.ac.jp>
2015-06-01 20:13:52 +0900

    token用の記述追加
    index.js編集　token用の記述追加

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 'use strict';
 var fs=require('fs');
 
+var querystring=require('querystring');
+
 console.log("Hello");
 if(process .argv[2]===undefined){
 	console.log('argv:',process.argv);
@@ -13,4 +15,45 @@ if(process .argv[2]==='init'){
 	//installed
 	var config=JSON.parse(fs.readFileSync(',/config.json')).installed;
 	console.log(config);
-}{
+}
+if(process .argv[2]==='auth'){
+	//installed
+	var config=JSON.parse(fs.readFileSync(',/config.json')).installed;
+	var parms={
+		response_type:'code',
+		access_type:'offline',
+		approval_prompt:'force',
+		client_id:config.client_id,	//configƒtƒ@ƒCƒ‹‚©‚çidŽæ‚Á‚Ä‚­‚é
+		redirect_uri:config.redirect_uris[0],
+		scope:'https://www.googleapis.com/auth/gmail.labels',
+		state:'some random string haha'
+	}
+	var uri=config.auth_uri+'?'+querystring.encode(parms);
+	console.log(uri);
+}
+
+if (process.argv[2]==='token'){
+	var config=JSON.parse(fs.readFileSync(',/config.json')).installed;
+	var code= readline/question('Input returned code:');
+	var parms={
+		grant_type:'authrization_code',
+		code: code,
+		client_id:config.client_id,
+		client_secret:config.client_secret,
+		redirect_uri:config.redirect_uris[0]
+	};
+	var options={
+		uri:config.token_uri,
+		form:parms,
+		json:true
+	};
+	request.post(options,function(error,response,body){
+		if(responce.statusCode!==200){
+			console.log("Error:",error);
+			console.log("Status code:",response.statusCode);
+			console.log("Body:",body);
+			return false;
+		}
+		fs.writeFileSync(',/token.json',JSON.stringify(body));
+	});
+}
